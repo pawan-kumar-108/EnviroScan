8430e6f962f719d3ab89506c3ccfa228163eacbc
Pawan Kumar <126077105+pawan-kumar-108@users.noreply.github.com>
2024-11-09 00:24:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,28 @@ import api  # Import the API routes from api.py
 app = Flask(__name__)
 CORS(app)  # Enable Cross-Origin Resource Sharing for all routes
 
-#Register the routes from api.py with the app
+# Register the routes from api.py with the app
 app.register_blueprint(api.bp)
 
-if __name__ == "__main":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+else:
+    # Use Gunicorn to run the application
+    import gunicorn
+    from gunicorn.app.base import Application
+
+    class FlaskApplication(Application):
+        def __init__(self, app):
+            self.application = app
+            super().__init__()
+
+        def load_config(self):
+            self.cfg.set("worker_class", "gevent")
+            self.cfg.set("bind", "0.0.0.0:5000")
+            self.cfg.set("pathConfig", "")
+
+        def load(self):
+            return self.application
+
+    if __name__ == "__main__":
+        FlaskApplication(app).run()
